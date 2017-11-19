fc1d6b4500ee288d88c5f96031a1c7ea88430182
Praveen <kprkumar528@gmail.com>
2017-11-19 09:43:01 +0000

    Initial shared libs commit

---
+++ vars/E2E-sharedlib.groovy
@@ -0,0 +1,35 @@
+import groovy.json.JsonSlurper
+def call(body) 
+{
+    def config =[:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    node
+    {
+	stage('Build')
+	{
+		print "Build stage steps here..."
+	}
+	stage('Test')
+	{
+		print "Test stage steps here..."
+	}
+	stage('Dev')
+	{
+		print "Dev stage steps here..."
+	}
+	stage('Preprod')
+	{
+		print "Preprod stage steps here..."
+	}
+	stage('Manual approval')
+	{
+		input 'Can this be promoted to Prod?'
+	}
+	stage('Prod')
+	{
+		print 'Prod stage steps here...
+	}
+    }
+}
