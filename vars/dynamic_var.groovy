c79e8da6385005fa9ed09d437e027bd4cba37ea2
Praveen Kandregula <kprkumar528@gmail.com>
2018-06-20 11:32:54 +0000

    New lib to test dynamic param

---
+++ vars/dynamic_var.groovy
@@ -0,0 +1,10 @@
+import groovy.json.JsonSlurper
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+   def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    println "${config}"
