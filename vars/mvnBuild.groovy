e2cfcfb830075ecbcc3f18b0e1b79eb591e61dcd
Praveen Kandregula <kprkumar528@gmail.com>
2020-03-01 19:00:04 +0000

    Fixing dir path

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -5,7 +5,7 @@ String call(mavenGoal){
     rtMaven.deployer.artifactDeploymentPatterns.addInclude("*.jar")
     rtMaven.tool = 'maven 3.6.3'
     print pwd()
-    //def buildInfo = rtMaven.run pom: 'my-app/pom.xml', goals: mavenGoal
+    //def buildInfo = rtMaven.run pom: './pom.xml', goals: mavenGoal
     rtMaven.run pom: 'my-app/pom.xml', goals: 'package'
     //rtMaven.deployer.deployArtifacts buildInfo
     //server.publishBuildInfo buildInfo
