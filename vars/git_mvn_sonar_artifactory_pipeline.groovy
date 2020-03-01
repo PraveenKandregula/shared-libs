b2ab9a421a726bd962bc30e9de9ccc5333c506d6
Praveen Kandregula <kprkumar528@gmail.com>
2020-03-01 16:36:46 +0000

    Added maven build stage

--- vars/git_mvn_sonar_artifactory_pipeline.groovy
+++ vars/git_mvn_sonar_artifactory_pipeline.groovy
@@ -6,9 +6,19 @@ def call(body)
     body.delegate = config
     body()
     def scm_url
+    def ismvnProject
+    def mavenStep
     node{
 	stage('scm-checkout'){
             gitCheckout.call("${config.scm_url}")
 	}
+        stage('mvn-build'){
+            script{
+                if ("${config.ismvnProject}")
+		    mvnBuild.call("${config.mavenStep}")
+                else
+                    print "This is not maven project"
+            }
+        }
     }
 }
