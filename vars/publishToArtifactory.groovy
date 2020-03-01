741abc9dca17dfba8976e61e67a21c2048185750
Praveen Kandregula <kprkumar528@gmail.com>
2020-03-01 20:33:14 +0000

    publishToArtifactory stage

--- vars/publishToArtifactory.groovy
+++ vars/publishToArtifactory.groovy
@@ -2,7 +2,7 @@ String call(){
     def server = Artifactory.server('artifactory')
     def uploadSpec = """{
 	                 "files": [{
-	                            "pattern": "target/*.jar",
+	                            "pattern": "target/\\*.jar",
 		                    "target": "example-repo-local/${currentBuild.displayName}",
       		                    "recursive": "true",
 		                    "regexp": "true"
