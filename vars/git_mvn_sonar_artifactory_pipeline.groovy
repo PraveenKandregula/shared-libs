a5f508415e75c8abc2b9c8a4c4c1bd6becf4ced5
Praveen Kandregula <kprkumar528@gmail.com>
2020-03-01 15:34:17 +0000

    Global pipeline for git checkout,maven build,sonar scan and artifactory upload

---
+++ vars/git_mvn_sonar_artifactory_pipeline.groovy
@@ -0,0 +1,13 @@
+import groovy.json.JsonSlurper
+def call(body) 
+{
+    def config =[:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    def scm_url
+    node{
+	stage('scam-checkout'){
+            gitCheckout()
+	}
+    }
