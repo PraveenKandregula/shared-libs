f602e13edaf65cd6c9537073eee5b4d72ccc6a19
Praveen Kandregula <kprkumar528@gmail.com>
2020-03-01 17:48:10 +0000

    mavenGoal in script directive

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,11 +1,10 @@
 String call(mavenGoal){
-    //steps{
-        //script{
-            sh'''
-             cd my-app
-             pwd
-             $mavenGoal
-            '''
-        //}
-    //}
+    def server = Artifactory.server('artifactory')
+    def rtMaven = Artifactory.newMavenBuild()
+    rtMaven.deployer server: server, releaseRepo: 'example-repo-local', snapshotRepo: 'example-repo-local'
+    rtMaven.deployer.artifactDeploymentPatterns.addInclude("*.jar")
+    rtMaven.tool = 'maven'
+    def buildInfo = rtMaven.run pom: 'maven-example/pom.xml', goals: mavenGoal
+    rtMaven.deployer.deployArtifacts buildInfo
+    server.publishBuildInfo buildInfo
 }
