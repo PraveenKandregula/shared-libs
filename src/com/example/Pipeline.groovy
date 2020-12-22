18a181c5d9a6e8dba5051b3369003172adbba61a
Cloud User <ec2-user@ip-172-31-33-7.us-east-2.compute.internal>
2020-12-22 09:52:14 +0000

    New pipeline code

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -4,15 +4,16 @@ class Pipeline {
     def script
     def configurationFile
 
-    Pipeline(script, configurationFile) {
-        this.script = script
-        this.configurationFile = configurationFile
-    }
+    //Pipeline(script, configurationFile) {
+    //    this.script = script
+    //    this.configurationFile = configurationFile
+    //}
 
     def execute() {
 	//System.out.println $script
 	//System.out.println $configurationFile
-	echo "Script: ${script.dump()}"
-	echo "Config file: ${configurationFile.dump()}"
+	//echo "Script: ${script.dump()}"
+	//echo "Config file: ${configurationFile.dump()}"
+	echo "This is execute method"
     }
 }
