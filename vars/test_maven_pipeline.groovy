f0afe0423b1ce4424bac4fcbaa56cebf52bbc50c
Cloud User <ec2-user@ip-172-31-33-7.us-east-2.compute.internal>
2020-12-22 17:49:06 +0000

    New pipeline code

--- vars/test_maven_pipeline.groovy
+++ vars/test_maven_pipeline.groovy
@@ -11,7 +11,7 @@ def call(body)
 	stage('Checkout'){
 	    echo "Checking out ${config.git_repo_url}"
 	    //checkout scm: [$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: git_repo_url]]]
-	    checkout([$class: 'GitSCM', doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'scm_credential', refspec: '+refs/heads/master:refs/remotes/origin/master', url: config.git_repo_url]]])
+	    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[credentialsId: 'scm_credential', url: config.git_repo_url]]])
 	}
 	
     }
